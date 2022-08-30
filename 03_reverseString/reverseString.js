07f01dab80914eb4cebcbc0218054a071bb00c07
jon-krs <jonkrs53@gmail.com>
2022-08-30 15:11:32 -0700

    Add solutions to js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const strArray = string.split('');
+    let newString = '';
+    for (let i = 0; i < string.length; i++) {
+        newString += strArray.pop();
+    }
+    return newString;
 };
 
 // Do not edit below this line
