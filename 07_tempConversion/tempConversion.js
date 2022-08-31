5b3bd343ac22f60cb90349af5df1b88fb7cd6e82
jon-krs <jonkrs53@gmail.com>
2022-08-31 13:13:05 -0700

    Add solutions to js exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp;
+  cTemp = (fTemp - 32) * (5/9);
+  return +cTemp.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp;
+  fTemp = (cTemp * (9/5)) + 32;
+  return +fTemp.toFixed(1);
 };
 
 // Do not edit below this line
